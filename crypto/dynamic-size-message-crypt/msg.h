c3fea9db7af664b8c353cea507bb40274251d70b
ryxxn <ryoon50@gmail.com>
2024-12-05 03:31:17 +0000

    fix

--- crypto/dynamic-size-message-crypt/msg.h
+++ crypto/dynamic-size-message-crypt/msg.h
@@ -4,14 +4,14 @@
 #include <openssl/aes.h>
 
 #define AES_KEY_128 16
-#define BUFSIZE 256
+#define BUF_SIZE 256
 
 
 typedef struct _APP_MSG_
 {
   int type;
   // 가변 크기를 위해 포인터에 저장
-  unsigned char payload[BUFSIZE + AES_BLOCK_SIZE];
+  unsigned char payload[BUF_SIZE + AES_BLOCK_SIZE];
   int msg_len;
 }APP_MSG;
 
