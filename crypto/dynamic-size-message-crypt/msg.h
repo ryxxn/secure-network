7df480e16230a8600ca0b576d01cf2757f6f732f
ryxxn <ryoon50@gmail.com>
2024-12-05 03:23:54 +0000

    fix

--- crypto/dynamic-size-message-crypt/msg.h
+++ crypto/dynamic-size-message-crypt/msg.h
@@ -4,11 +4,13 @@
 #define AES_KEY_128 16
 #define BUFSIZE 256
 
+#include <openssl/aes.h>
+
 typedef struct _APP_MSG_
 {
   int type;
   // 가변 크기를 위해 포인터에 저장
-  unsigned char payload[BUFSIZE + AES_KEY_128];
+  unsigned char payload[BUFSIZE + AES_BLOCK_SIZE];
   int msg_len;
 }APP_MSG;
 
